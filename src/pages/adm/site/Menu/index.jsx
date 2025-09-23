ed00c91568859f20b2d401674243765ddf831114
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2025-09-22 22:41:39 -0300

    menu gestao

--- src/pages/adm/site/Menu/index.jsx
+++ src/pages/adm/site/Menu/index.jsx
@@ -1,10 +1,26 @@
+import { Link } from "react-router-dom";
+import logo from '../../../../assets/logo2.png';
 import './style.css';
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 export default function MenuGestao() {
-
-  return (
-    <div >
-
-    </div>
-  );
+    return (
+        <div>
+            <div className="inicio-menug">
+                <div className="titulo-menug">
+                    <img src={logo} alt="Logo" />
+                    <p>Gestão Site Instituto</p>
+                </div>
+                <Link to="/inicio-adm" className="voltar-adm" >Voltar <FaLongArrowAltRight /></Link>
+            </div>
+            <p className="p-opcoes">Bem-vindo! Escolha uma das opções abaixo a editar:</p>
+            <div className="opcoes-menug">
+                <Link to="/galeria-gestao">GALERIA</Link>
+                <Link to="/eventos-gestao">EVENTOS</Link>
+                <Link to="/projetos-de-cursos-gestao">PROJETOS E OFICINAS</Link> 
+                <Link to="/comunidade-gestao">PROJETOS SOCIAIS E VOLUNTARIADO</Link>   
+                <Link to="/cursos-gestao">CURSOS</Link>                  
+            </div>
+        </div>
+    );
 }
