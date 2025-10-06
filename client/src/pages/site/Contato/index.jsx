e5d393fa4150d9e03de062bf6b7c94e589776a31
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2025-10-06 14:47:21 -0300

    contato

--- client/src/pages/site/Contato/index.jsx
+++ client/src/pages/site/Contato/index.jsx
@@ -11,20 +11,17 @@ export default function Contato() {
         <Header />
         <main>
           <h1>Contato</h1>
-          <p>Tem alguma dúvida ou sugestão.<br/>Entre em contato conosco agora mesmo!</p>
-          <div>
-            <div>
+          <p className="subtitulo-contato">Tem alguma dúvida ou sugestão.<br/>Entre em contato conosco agora mesmo!</p>
+          <div className="container-contato">
+            <div className="opcoes-contato">
               <div>
-                <FaPhone />
-                <p>(42) 3624-2318</p>
+                <p><FaPhone /> (42) 3624-2318</p>
               </div>
               <div>
-                <MdEmail />
-                <p>contatos@dombosco.net</p>
+                <p><MdEmail /> contatos@dombosco.net</p>
               </div>
               <div>
-                <IoIosPin />
-                <p>R. Padre Caetano Vendrami, 303 - Vila Carli, Guarapuava - PR, 85040-050</p>
+                <p><IoIosPin /> R. Padre Caetano Vendrami, 303 - Vila Carli, Guarapuava - PR, 85040-050</p>
               </div>
             </div>
             <form id="form-contato" className="form-contato">
@@ -36,15 +33,15 @@ export default function Contato() {
               <button type="submit">Enviar</button>
             </form>
           </div>
-          <div>
+          <div className="div-mapa">
             <h2>Encontre-nos no Google Maps</h2>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3604.848202047173!2d-51.49376965952778!3d-25.376403477689262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ef37a39bf787a3%3A0xf95e737a46ee9a1c!2sInstituto%20Assistencial%20Dom%20Bosco!5e0!3m2!1spt-BR!2sbr!4v1759755000061!5m2!1spt-BR!2sbr" 
-            width="600" 
-            height="450" 
-            style={{border: 0}} 
-            allowfullscreen
-            loading="lazy" 
-            referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div className="mapa-container">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3604.848202047173!2d-51.49376965952778!3d-25.376403477689262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ef37a39bf787a3%3A0xf95e737a46ee9a1c!2sInstituto%20Assistencial%20Dom%20Bosco!5e0!3m2!1spt-BR!2sbr!4v1759755000061!5m2!1spt-BR!2sbr" 
+              allowfullscreen
+              loading="lazy" 
+              referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            
           </div>      
         </main>
         <Footer />
