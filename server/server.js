472d25f420d4694c72e9534b1af0befac81dc1a7
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2026-03-22 10:06:55 -0300

    test

--- server/server.js
+++ server/server.js
@@ -54,17 +54,17 @@ import express from "express";
 import dotenv from "dotenv";
 import corsMiddleware from "./src/middlewares/cors.js";
 import authRoutes from "./src/api/auth.js";
-// import depoimentosRoutes from "./src/api/depoimentos.js";
-// import equipeRoutes from "./src/api/equipe.js";
-// import emailRoutes from "./src/api/email.js";
-// import galeriaRoutes from "./src/api/galeria.js";
-// import cursosRoutes from "./src/api/cursos.js";
-// import usuariosRoutes from "./src/api/usuarios.js";
-// import videosRoutes from "./src/api/videos.js";
-// import refeicoesRoutes from "./src/api/refeicoes.js";
-// import relatoriosRoutes from "./src/api/relatorios.js";
-// import tarefasRoutes from "./src/api/tarefas.js";
-// import balancaRoutes from "./src/api/balanca.js";
+import depoimentosRoutes from "./src/api/depoimentos.js";
+import equipeRoutes from "./src/api/equipe.js";
+import emailRoutes from "./src/api/email.js";
+import galeriaRoutes from "./src/api/galeria.js";
+import cursosRoutes from "./src/api/cursos.js";
+import usuariosRoutes from "./src/api/usuarios.js";
+import videosRoutes from "./src/api/videos.js";
+import refeicoesRoutes from "./src/api/refeicoes.js";
+import relatoriosRoutes from "./src/api/relatorios.js";
+import tarefasRoutes from "./src/api/tarefas.js";
+import balancaRoutes from "./src/api/balanca.js";
 
 
 dotenv.config();
@@ -83,16 +83,16 @@ app.get("/api/teste", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/depoimentos", depoimentosRoutes);
-// app.use("/api/equipe", equipeRoutes);
-// app.use("/api/email", emailRoutes);
-// app.use("/api/galeria", galeriaRoutes);
-// app.use("/api/cursos", cursosRoutes);
-// app.use("/api/usuarios", usuariosRoutes);
-// app.use("/api/videos", videosRoutes);
-// app.use("/api/refeicoes", refeicoesRoutes);
-// app.use("/api/relatorios", relatoriosRoutes);
-// app.use("/api/tarefas", tarefasRoutes);
-// app.use("/api/pesagem", balancaRoutes);
+app.use("/api/depoimentos", depoimentosRoutes);
+app.use("/api/equipe", equipeRoutes);
+app.use("/api/email", emailRoutes);
+app.use("/api/galeria", galeriaRoutes);
+app.use("/api/cursos", cursosRoutes);
+app.use("/api/usuarios", usuariosRoutes);
+app.use("/api/videos", videosRoutes);
+app.use("/api/refeicoes", refeicoesRoutes);
+app.use("/api/relatorios", relatoriosRoutes);
+app.use("/api/tarefas", tarefasRoutes);
+app.use("/api/pesagem", balancaRoutes);
 
 export default app;
