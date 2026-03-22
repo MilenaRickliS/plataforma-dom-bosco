8ac108ad555c431fafc9e6b61edf92a18b73445b
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2026-03-22 09:14:20 -0300

    test

--- server/server.js
+++ server/server.js
@@ -53,7 +53,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import corsMiddleware from "./src/middlewares/cors.js";
-import authRoutes from "./src/api/auth.js";
+// import authRoutes from "./src/api/auth.js";
+import depoimentosRoutes from "./src/api/depoimentos.js";
+// import equipeRoutes from "./src/api/equipe.js";
+// import emailRoutes from "./src/api/email.js";
+// import galeriaRoutes from "./src/api/galeria.js";
+// import cursosRoutes from "./src/api/cursos.js";
+// import usuariosRoutes from "./src/api/usuarios.js";
+// import videosRoutes from "./src/api/videos.js";
+// import refeicoesRoutes from "./src/api/refeicoes.js";
+// import relatoriosRoutes from "./src/api/relatorios.js";
+// import tarefasRoutes from "./src/api/tarefas.js";
+// import balancaRoutes from "./src/api/balanca.js";
 
 
 dotenv.config();
@@ -70,6 +81,18 @@ app.get("/", (req, res) => {
 app.get("/api/teste", (req, res) => {
   res.status(200).json({ ok: true, rota: "teste funcionando" });
 });
-app.use("/api/auth", authRoutes);
+
+// app.use("/api/auth", authRoutes);
+app.use("/api/depoimentos", depoimentosRoutes);
+// app.use("/api/equipe", equipeRoutes);
+// app.use("/api/email", emailRoutes);
+// app.use("/api/galeria", galeriaRoutes);
+// app.use("/api/cursos", cursosRoutes);
+// app.use("/api/usuarios", usuariosRoutes);
+// app.use("/api/videos", videosRoutes);
+// app.use("/api/refeicoes", refeicoesRoutes);
+// app.use("/api/relatorios", relatoriosRoutes);
+// app.use("/api/tarefas", tarefasRoutes);
+// app.use("/api/pesagem", balancaRoutes);
 
 export default app;
