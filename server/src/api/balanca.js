191912d9f4f5e006cbbea73c538a054eb9700bc0
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2026-08-23 11:31:28 -0300

    test

--- server/src/api/balanca.js
+++ server/src/api/balanca.js
@@ -1,3 +1,11 @@
+console.log("========== DELETE REGISTRO ==========");
+console.log("Método:", req.method);
+console.log("Tipo:", req.query.tipo);
+console.log(
+  "SENHA_EXCLUSAO_BALANCA configurada:",
+  !!process.env.SENHA_EXCLUSAO_BALANCA
+);
+console.log("====================================");
 import admin from "../firebaseAdmin.js";
 const db = admin.firestore();
 const parseRangeDate = (value, mode) => {
